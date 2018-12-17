e2e31e6ca7772f1f182cf20a89041c1466e07bd5
Sean Houghton <sbn.houghton@gmail.com>
2018-12-17 16:52:19 -0500

    Update mixcrSSM.py
    immunotools version 4

--- pipelines/mixcrSSM.py
+++ pipelines/mixcrSSM.py
@@ -62,16 +62,16 @@ def mixcrSSM(file_list, s3_output):
 			fastq1_transfer = "aws s3 cp {} /home/ec2-user/fastq_RNA/{}".format(aws_fastq1, fastq1)
 			fastq2_transfer = "aws s3 cp {} /home/ec2-user/fastq_RNA/{}".format(aws_fastq2, fastq2)
       
-			trimmomatic = "sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version3 java -jar /home/tools/Trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 /data/fastq_RNA/{} /data/fastq_RNA/{} /data/fastq_RNA/{} /data/fastq_RNA/{} /data/fastq_RNA/{} /data/fastq_RNA/{} ILLUMINACLIP:/home/tools/Trimmomatic-0.36/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:6 MINLEN:28".format(fastq1,fastq2,fastq1_trimmed,fastq1_trimmed_unpaired,fastq2_trimmed,fastq2_trimmed_unpaired)
+			trimmomatic = "sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version4 java -jar /home/tools/Trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 /data/fastq_RNA/{} /data/fastq_RNA/{} /data/fastq_RNA/{} /data/fastq_RNA/{} /data/fastq_RNA/{} /data/fastq_RNA/{} ILLUMINACLIP:/home/tools/Trimmomatic-0.36/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:6 MINLEN:28".format(fastq1,fastq2,fastq1_trimmed,fastq1_trimmed_unpaired,fastq2_trimmed,fastq2_trimmed_unpaired)
 			
-			mixcr = "sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version3 /home/tools/mixcr/mixcr align -p rna-seq -s hsa -OallowPartialAlignments=true -r log.v3.txt /data/fastq_RNA/{} /data/fastq_RNA/{} data/output/T_Cell.v3.alignments.vdjca".format(fastq1_trimmed, fastq2_trimmed)
-			mixcr_step1 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version3 /home/tools/mixcr/mixcr assemblePartial data/output/T_Cell.v3.alignments.vdjca data/output/T_Cell.v3.alignments_rescued_1.vdjca'
-			mixcr_step2 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version3 /home/tools/mixcr/mixcr assemblePartial data/output/T_Cell.v3.alignments_rescued_1.vdjca data/output/T_Cell.v3.alignments_rescued_2.vdjca'
-			mixcr_step3 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version3 /home/tools/mixcr/mixcr extend data/output/T_Cell.v3.alignments_rescued_2.vdjca data/output/T_Cell.v3.alignments_rescued_2_extended.vdjca'
-			mixcr_step4 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version3 /home/tools/mixcr/mixcr assemble data/output/T_Cell.v3.alignments_rescued_2_extended.vdjca data/output/T_Cell.v3.clones.clns'
-			mixcr_step5 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version3 /home/tools/mixcr/mixcr exportClones -c TRB data/output/T_Cell.v3.clones.clns data/output/T_Cell.v3.clones.TRB.txt'
-			mixcr_step6 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version3 /home/tools/mixcr/mixcr exportClones -c TRA data/output/T_Cell.v3.clones.clns data/output/T_Cell.v3.clones.TRA.txt'
-			mixcr_step7 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version3 /home/tools/mixcr/mixcr exportClones -c IGH data/output/T_Cell.v3.clones.clns data/output/T_Cell.v3.clones.IGH.txt'
+			mixcr = "sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version4 /home/tools/mixcr/mixcr align -p rna-seq -s hsa -OallowPartialAlignments=true -r log.v3.txt /data/fastq_RNA/{} /data/fastq_RNA/{} data/output/T_Cell.v3.alignments.vdjca".format(fastq1_trimmed, fastq2_trimmed)
+			mixcr_step1 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version4 /home/tools/mixcr/mixcr assemblePartial data/output/T_Cell.v3.alignments.vdjca data/output/T_Cell.v3.alignments_rescued_1.vdjca'
+			mixcr_step2 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version4 /home/tools/mixcr/mixcr assemblePartial data/output/T_Cell.v3.alignments_rescued_1.vdjca data/output/T_Cell.v3.alignments_rescued_2.vdjca'
+			mixcr_step3 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version4 /home/tools/mixcr/mixcr extend data/output/T_Cell.v3.alignments_rescued_2.vdjca data/output/T_Cell.v3.alignments_rescued_2_extended.vdjca'
+			mixcr_step4 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version4 /home/tools/mixcr/mixcr assemble data/output/T_Cell.v3.alignments_rescued_2_extended.vdjca data/output/T_Cell.v3.clones.clns'
+			mixcr_step5 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version4 /home/tools/mixcr/mixcr exportClones -c TRB data/output/T_Cell.v3.clones.clns data/output/T_Cell.v3.clones.TRB.txt'
+			mixcr_step6 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version4 /home/tools/mixcr/mixcr exportClones -c TRA data/output/T_Cell.v3.clones.clns data/output/T_Cell.v3.clones.TRA.txt'
+			mixcr_step7 = 'sudo docker run -v /home/ec2-user/:/data houghtos/immunotools:version4 /home/tools/mixcr/mixcr exportClones -c IGH data/output/T_Cell.v3.clones.clns data/output/T_Cell.v3.clones.IGH.txt'
 
 			copy = "aws s3 cp /home/ec2-user/output/ " + "s3://" + bucket + "/{}/{}/ --recursive".format(s3_output,star_output_prefix[:-1])
 			removeFastqFolder = "sudo rm -rf /home/ec2-user/fastq_RNA/"
